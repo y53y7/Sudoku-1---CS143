942c94f15397f0b4bf826ba3f8fd4aa2260a2393
y53y7 <maxwell.sutton@go.shoreline.edu>
2025-04-23 19:51:22 -0700

    Finalized the constructor and tostring

--- SudokuBoard.java
+++ SudokuBoard.java
@@ -1,11 +1,35 @@
+import java.io.*;
+import java.util.*;
+
 public class SudokuBoard {
    private char[][] board;
    
    public SudokuBoard(String filename) {
-      
+      try {
+         Scanner scan = new Scanner(new File(filename));
+         board = new char[9][9];
+         for(int r = 0; r < board.length; r++) {
+            if(scan.hasNextLine()) {
+               String line = scan.nextLine();
+               for(int c = 0; c < board[0].length; c++) {
+                  board[r][c] = line.charAt(c);
+               }
+            }
+         }
+      } catch(FileNotFoundException e) {
+         System.out.println("Cannot find " + filename);
+      }
    }
    
    public String toString() {
-      return "";
+      String s = "";
+      for(int r = 0; r < board.length; r++) {
+         for(int c = 0; c < board[0].length; c++) {
+            s += board[r][c] + " ";
+         }
+      s += "\n";
+      }
+      
+      return s;
    }
 }
---
+++ SudokuEngine.java
@@ -0,0 +1,6 @@
+public class SudokuEngine {
+    public static void main(String[] args) {
+        SudokuBoard myBoard = new SudokuBoard("data1.sdk");
+        System.out.println(myBoard);
+    }
+}
